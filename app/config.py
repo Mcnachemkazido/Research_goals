3a04ea2f9a46bddee781ebbe9bebf533a3992a13
MenachemKazido <agbg7172@gmail.com>
2026-03-17 10:32:44 +0200

    crate basic connetion to db

---
+++ app/config.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class DbConfig:
+
+    @staticmethod
+    def get_host():
+        if os.getenv("SQL_HOST"):
+            return os.getenv("SQL_HOST")
+        return False
+
+    @staticmethod
+    def get_port():
+        if os.getenv("SQL_PORT"):
+            return os.getenv("SQL_PORT")
+        return False
+
+    @staticmethod
+    def get_user():
+        if os.getenv("SQL_USER"):
+            return os.getenv("SQL_USER")
+        return False
+
+    @staticmethod
+    def get_password():
+        if os.getenv("SQL_PASSWORD"):
+            return os.getenv("SQL_PASSWORD")
+        return False
+
+    @staticmethod
+    def get_db():
+        if os.getenv("SQL_DB"):
+            return os.getenv("SQL_DB")
+        return False
---
+++ app/db/connection.py
@@ -0,0 +1,24 @@
+import pymysql
+
+class Connection:
+    def __init__(self,host,port,user,password,db):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db = db
+
+    def get_conn(self):
+        try:
+            return pymysql.connect(
+                host=self.host,
+               port=self.port,
+               user=self.user,
+               password=self.password)
+
+        except pymysql.err.OperationalError as e :
+            print(e)
+            raise
+
+conn = Connection("localhost",3306,"root","root","digital_hunter")
+print(conn.get_conn())
